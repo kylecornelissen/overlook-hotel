66a30797d3c0b06ce8177261f8ee082ae3eebdce
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-01-14 12:58:47 -0700

    Add moneySpent domUpdate method

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 
 const domUpdates = {
   insertLoginError() {
+    $('#error-msg').css({'color': 'red', 'transition': '.8s ease'});
     $('#error-msg').empty().append('Username Does Not Exist.');
   },
   insertPasswordError() {
+    $('#error-msg').css({'color': 'red', 'transition': '.8s ease'});
     $('#error-msg').empty().append('Password is Incorrect.');
   },
   insertGuestName(user) {
@@ -12,11 +14,19 @@ const domUpdates = {
     $('#welcome-heading').append(`Hello ${name}...`);
   },
   addBookings(user, bookings) {
-    user.findBookings(bookings).forEach(b => {;
-      $('#booking-list').append(
-        `<li>Room ${b.roomNum} on ${b.date}</li>`
-      )
+    user.findBookings(bookings)
+    $('#booking-list').empty();
+    user.bookings.forEach(b => {
+      $('#booking-list').append(`<li>Room ${b.roomNum} on ${b.date}</li>`)
     });
+    if ($('#booking-list')[0].children.length === 0) this.addBookings(user,bookings);
+  },
+  addMoneySpent(user, rooms) {
+    let totalCost = user.bookings.reduce((acc,b) => {
+      acc += b.findTotalRoomCost(rooms);
+      return acc
+    }, 0);
+    $('#total-spending').append(`Total Money Spent On Rooms: $${totalCost}`);
   }
 }
 
