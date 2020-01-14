318ece9b49976b6d8222fde411a0cfca7a21427a
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-01-13 18:38:04 -0700

    Add room class with test

---
+++ src/classes/Room.js
@@ -0,0 +1,12 @@
+class Room {
+  constructor(num, type, bidet, bedSize, numBeds, cost) {
+    this.num = num;
+    this.type = type;
+    this.bidet = bidet;
+    this.bedSize = bedSize;
+    this.numBeds = numBeds;
+    this.cost = cost;
+  }
+}
+
+export default Room;
