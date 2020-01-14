6564e1f1ef458b7b82954bd9f0dc1efcbb58993b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-01-14 11:14:05 -0700

    Add setupGuestDashboard method

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ $('.login-btn').click(verifyLogin);
 // $('.logout-btn').click(verifyLoginInfo); //redirect to login page and clear storage
 
 function findPage() {
-  if (page[0].title === 'Gerard Hotel') {fetchUsers()};
-  if (page[0].title === 'Guest Page') {  domUpdates.insertGuestName() };
+  if (page[0].title === 'Gerard Hotel') fetchUsers();
+  if (page[0].title === 'Guest Page') setupGuestDashboard();
   // if (page[0].title === 'Manager Page') {console.log('manager')};
 }
 
@@ -69,10 +69,11 @@ function verifyLogin() {
     domUpdates.insertLoginError();
     return
   } else if (username === 'manager') {
-    verifyPassword(password) ? loadManager(username) : domUpdates.insertPasswordError();
+    let manager = new Manager(); // future implementation might need to change this else if block
+    manager.verifyPassword(password) ? loadManager(username) : domUpdates.insertPasswordError();
   } else {
-    let guest = users.find(u => u.id === parseInt(username.substr(5,username.length-5)));
-    (!guest) ? domUpdates.insertLoginError() : (verifyPassword(password) ? loadGuest(guest) : domUpdates.insertPasswordError());
+    let guest = users.find(user => user.id === parseInt(username.substr(5,username.length-5)));
+    (!guest) ? domUpdates.insertLoginError() : (guest.verifyPassword(password) ? loadGuest(guest) : domUpdates.insertPasswordError());
   }
 };
 
@@ -83,4 +84,13 @@ function verifyPassword(password) {
 function loadGuest(guest) {
   window.localStorage.setItem('guest', JSON.stringify(guest));
   window.location.href = "guest-dashboard.html";
+}
+
+function setupGuestDashboard() {
+  let user = JSON.parse(window.localStorage.getItem('guest'));
+  let currentUser = new User(user.id, user.name);
+  fetchBookings();
+  domUpdates.insertGuestName(currentUser);
+  domUpdates.addBookings(currentUser, bookings);
+  setTimeout(() => domUpdates.addBookings(currentUser, bookings), 100);
 }
