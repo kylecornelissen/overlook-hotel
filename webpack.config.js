2f75e0b921c4957ac666630ae461cddb55949d63
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-01-15 01:06:16 -0700

    Add post fetch request methods for user to book a room

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html'
+      template: './src/index.html',
+      chunks: ['app']
     }),
     new HtmlWebpackPlugin({
       filename: 'guest-dashboard.html',
